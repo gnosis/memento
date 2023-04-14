24cdb600e132cbbaa6bb112f9499be6161adbcb3
Laurent De Marez <laurent@demarez.org>
2023-04-14 17:55:06 +0900

    chore: safe sdk

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => ([
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+  ]),
 }
 
 module.exports = nextConfig
--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import SafeProvider from "@safe-global/safe-apps-react-sdk";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <React.StrictMode>
+      <SafeProvider>
+        <Component {...pageProps} />
+      </SafeProvider>
+    </React.StrictMode>
+  );
 }
