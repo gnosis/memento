0847f9c275a0196b466db524f12c495c17668ecb
samepant <sampanter@gmail.com>
2023-04-15 20:26:58 +0900

    add base account wagmi type generation

--- packages/frontend/wagmi.config.ts
+++ packages/frontend/wagmi.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "@wagmi/cli";
 import { react } from "@wagmi/cli/plugins";
 import RecoveryArtifact from "../evm/artifacts/contracts/Recovery.sol/Recovery.json";
 import IEntrypointArtifact from "../evm/artifacts/@account-abstraction/contracts/interfaces/IEntryPoint.sol/IEntryPoint.json";
+import BaseAccounArtifact from "../evm/artifacts/@account-abstraction/contracts/core/BaseAccount.sol/BaseAccount.json";
 
 export default defineConfig({
   out: "generated.ts",
   contracts: [
     { name: "Recovery", abi: RecoveryArtifact.abi as any },
     { name: "IEntryPoint", abi: IEntrypointArtifact.abi as any },
+    { name: "BaseAccount", abi: BaseAccounArtifact.abi as any },
   ],
   plugins: [react()],
 });
