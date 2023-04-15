de91ac8c3711dbdd0fe8f12f69d47bdd8feda3d5
samepant <sampanter@gmail.com>
2023-04-15 19:51:25 +0900

    add entrypoint wagmi config

--- packages/frontend/wagmi.config.ts
+++ packages/frontend/wagmi.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "@wagmi/cli";
 import { react } from "@wagmi/cli/plugins";
 import RecoveryArtifact from "../evm/artifacts/contracts/Recovery.sol/Recovery.json";
+import IEntrypointArtifact from "../evm/artifacts/@account-abstraction/contracts/interfaces/IEntryPoint.sol/IEntryPoint.json";
 
 export default defineConfig({
   out: "generated.ts",
-  contracts: [{ name: "Recovery", abi: RecoveryArtifact.abi as any }],
+  contracts: [
+    { name: "Recovery", abi: RecoveryArtifact.abi as any },
+    { name: "IEntryPoint", abi: IEntrypointArtifact.abi as any },
+  ],
   plugins: [react()],
 });
